8b4683fab8c7e719cb432856583e48fc260b15eb
Marcio <marciogomes@alu.ufc.br>
2023-02-04 17:30:52 -0300

    initial commit

---
+++ Frontend/tellar-uxsoft-job/src/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.css";
+import "./global.css";
+
+export function App() {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <h1> Ave Maria! </h1>
+        <h1> Sacre cor Iesu</h1>
+      </div>
+    </>
+  );
+}
