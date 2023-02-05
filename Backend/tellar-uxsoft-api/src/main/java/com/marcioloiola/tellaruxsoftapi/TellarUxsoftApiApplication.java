d2ba9fd7bd58e445b7b4147093e5bd070be6516d
Marcio <marciogomes@alu.ufc.br>
2023-02-04 22:42:20 -0300

    backend initial commit

---
+++ Backend/tellar-uxsoft-api/src/main/java/com/marcioloiola/tellaruxsoftapi/TellarUxsoftApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcioloiola.tellaruxsoftapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellarUxsoftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TellarUxsoftApiApplication.class, args);
+	}
+
+}
---
+++ Backend/tellar-uxsoft-api/src/test/java/com/marcioloiola/tellaruxsoftapi/TellarUxsoftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcioloiola.tellaruxsoftapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TellarUxsoftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
